169aef148cd7e72c32bc8831257c62a3a0873ce1
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-02 13:35:33 +0545

    contains all hardcorded values

---
+++ config.js
@@ -0,0 +1,10 @@
+//We'll store hardcoded and global values in this file
+
+const GENESIS_DATA = {
+  timestamp: 1,
+  lastHash: "____",
+  hash: "hash-one",
+  data: [],
+};
+
+module.exports = { GENESIS_DATA };
