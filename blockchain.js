98242eb699eea848fcaf064ab5a429c23a75e379
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-02 16:27:49 +0545

    added chain validation fxn and test

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
 
 class Blockchain {
   constructor() {
@@ -12,6 +13,27 @@ class Blockchain {
     });
 
     this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+
+      const actualLastHash = chain[i - 1].hash;
+
+      const { timestamp, lastHash, hash, data } = block;
+
+      if (lastHash !== actualLastHash) return false;
+
+      const validatedHash = cryptoHash(timestamp, lastHash, data); //regenerating the hashes to compare | cryptoHash is a node module we've imported and set in "crypto-hash.js"
+
+      if (hash !== validatedHash) return false;
+    }
+    return true;
   }
 }
 
