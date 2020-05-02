ceb4f9e9dc659c367ea0f8c2792bbf5cd8935c04
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-02 13:58:52 +0545

    added mineBlock functionality

--- block.test.js
+++ block.test.js
@@ -26,4 +26,26 @@ describe("Block", () => {
       expect(genesisBlock).toEqual(GENESIS_DATA);
     });
   });
+
+  describe("mineBlock()", () => {
+    const lastBlock = Block.genesis();
+    const data = "mined_data";
+    const minedBlock = Block.mineBlock({ lastBlock, data });
+
+    it("returns a Block instance", () => {
+      expect(minedBlock instanceof Block).toBe(true);
+    });
+
+    it("sets the `lastHash` to be the `hash` of the lastBlock ", () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash); //actual_value goes in the expect() statement and expected value in toEqual() part
+    });
+
+    it("sets the `data`", () => {
+      expect(minedBlock.data).toEqual(data);
+    });
+
+    it("sets a `timestamp`", () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined); //the timestamp must exist
+    });
+  });
 });
