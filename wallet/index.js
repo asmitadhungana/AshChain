c07c4f00d603e40cebc4dc765ea87a9264c2741e
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-03 18:36:49 +0545

    signing data and verifying signatures fxnality

--- wallet/index.js
+++ wallet/index.js
@@ -1,13 +1,18 @@
 const { STARTING_BALANCE } = require("../config");
 const { ec } = require("../util");
+const cryptoHash = require("../util/crypto-hash");
 
 class Wallet {
   constructor() {
     this.balance = STARTING_BALANCE;
 
-    const keyPair = ec.genKeyPair(); //use genKeyPair() mthd in EC to generate the public and private keyPair
+    this.keyPair = ec.genKeyPair(); //use genKeyPair() mthd in EC to generate the public and private keyPair
 
-    this.publicKey = keyPair.getPublic().encode("hex");
+    this.publicKey = this.keyPair.getPublic().encode("hex"); //since the generated keys are in the form of points in ellipse, encode('hex') converts it to it's hex form
+  }
+
+  sign(data) {
+    return this.keyPair.sign(cryptoHash(data));
   }
 }
 
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require("./");
+const { verifySignature } = require("../util");
 
 describe("Wallet class", () => {
   let wallet;
@@ -12,8 +13,31 @@ describe("Wallet class", () => {
   });
 
   it("has a `publicKey`", () => {
-    console.log(wallet.publicKey);
-
+    // console.log(wallet.publicKey);
     expect(wallet).toHaveProperty("publicKey");
+  });
+
+  describe("signing data", () => {
+    const data = "ashbar";
+
+    it("verifies a signature", () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data),
+        })
+      ).toBe(true);
+    });
+
+    it("does not verify an invalid signature", () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data),
+        })
+      ).toBe(false);
+    });
   });
 });
