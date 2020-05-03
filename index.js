e12924e5c92553d97ef45aea31f7f9faf05d4a89
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-03 16:46:26 +0545

    configured api and ntk

---
+++ index.js
@@ -0,0 +1,59 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const request = require("request");
+const Blockchain = require("./blockchain");
+const PubSub = require("./pubsub");
+
+const app = express(); //make a local app object that's the result of calling this express fxn
+const blockchain = new Blockchain(); //create a main blockchain for the app [local blockchain instance set to that result]
+const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+app.use(bodyParser.json());
+
+app.get("/api/blocks", (req, res) => {
+  res.json(blockchain.chain); //this'll send back the blockchain.chain in its json form to whoever makes the /api/block request
+});
+
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({ data }); //this'll add a new block w/ the data from the requestor
+
+  pubsub.broadcastChain();
+
+  res.redirect("/api/blocks"); //give the requestor confirmation that their req was successful by showing the new block added to the chain
+});
+
+//to sync the nodes in the ntk
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+
+        console.log("replace chain on a sync with", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000); //generates a random value between 1 and 1000 for the  port
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT; //if peer port isn't defined, set default port (3000)
+app.listen(PORT, () => {
+  console.log(`listening at localhost:${PORT}`);
+
+  if (PORT !== DEFAULT_PORT) {
+    //don't send the synchronization request to the root node itself
+    syncChains();
+  }
+});
---
+++ pubsub.js
@@ -0,0 +1,105 @@
+const PubNub = require("pubnub");
+
+const credentials = {
+  //this will store all the keys and values we have in out pubnub application we created
+  publishKey: "pub-c-8fd5c4ba-aaaa-46a9-9d07-cf7f92d810a2",
+  subscribeKey: "sub-c-b0b93e88-8d19-11ea-8e98-72774568d584",
+  secretKey: "sec-c-MTgyOTJiYzQtZjkwYy00MDYwLThjMDEtZDUwYTdmNjJhYTQ3",
+};
+
+const CHANNELS = {
+  TEST: "TEST",
+  BLOCKCHAIN: "BLOCKCHAIN",
+  TRANSACTION: "TRANSACTION",
+};
+
+class PubSub {
+  constructor({ blockchain, transactionPool, wallet }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+
+    this.pubnub = new PubNub(credentials);
+
+    this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+
+    this.pubnub.addListener(this.listener());
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction),
+    });
+  }
+
+  subscribeToChannels() {
+    this.pubnub.subscribe({
+      channels: [Object.values(CHANNELS)],
+    });
+  }
+
+  listener() {
+    return {
+      message: (messageObject) => {
+        const { channel, message } = messageObject;
+
+        console.log(
+          `Message received. Channel: ${channel}. Message: ${message}`
+        );
+        const parsedMessage = JSON.parse(message);
+
+        switch (channel) {
+          case CHANNELS.BLOCKCHAIN:
+            this.blockchain.replaceChain(parsedMessage, true, () => {
+              this.transactionPool.clearBlockchainTransactions({
+                chain: parsedMessage.chain,
+              });
+            });
+            break;
+          case CHANNELS.TRANSACTION:
+            if (
+              !this.transactionPool.existingTransaction({
+                inputAddress: this.wallet.publicKey,
+              })
+            ) {
+              this.transactionPool.setTransaction(parsedMessage);
+            }
+            break;
+          default:
+            return;
+        }
+      },
+    };
+  }
+
+  publish({ channel, message }) {
+    // there is an unsubscribe function in pubnub
+    // but it doesn't have a callback that fires after success
+    // therefore, redundant publishes to the same local subscriber will be accepted as noisy no-ops
+    this.pubnub.publish({ message, channel });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction),
+    });
+  }
+}
+
+module.exports = PubSub;
