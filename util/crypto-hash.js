60bbcc0294e437a787d4920238609c9f95c30d58
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-04 11:46:49 +0545

    improved the cryptohash fxn to work on stringified inputs to prevent same object-diff reference js dielemma

--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -3,7 +3,13 @@ const crypto = require("crypto"); //node module [has createHash() fxn]
 const cryptoHash = (...inputs) => {
   const hash = crypto.createHash("sha256"); //we want sha256
 
-  hash.update(inputs.sort().join(" "));
+  hash.update(
+    inputs
+      .map((input) => JSON.stringify(input))
+      .sort()
+      .join(" ")
+  ); //mapping into the inputs and turning all the inner items to their stringified forms
+  //by stringifying all the inputs, we'll make sure that if an object's property has changed, the stringified form will represent those changes
 
   return hash.digest("hex"); //digest=[a term in cryptography to represent the result of the hash] | in hex form
 };
--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -2,14 +2,24 @@ const cryptoHash = require("./crypto-hash");
 
 describe("cryptoHash()", () => {
   it("generates a SHA-256 hashed output", () => {
-    expect(cryptoHash("asmee")).toEqual(
-      "5e2b803afad14d5c26946a21134e3765fb723c0002afdb004c41f7790f26fc9b"
-    ); //hash was gen. using SHA-256 online generator for "asmee"
+    expect(cryptoHash("ash")).toEqual(
+      "15e105b2bf800a41d47f7310503b6975cc960457c3ebddd8d66954f2d1f5b970"
+    ); //hash was gen. using SHA-256 online generator for "ash" [including the double quotes since our stringified version of input is hashed now]
   });
 
   it("produces the same hash with the same input arguments inputted in any order", () => {
     expect(cryptoHash("one", "two", "three")).toEqual(
       cryptoHash("two", "three", "one")
     );
+  });
+
+  it("produces a unique hash when the properties have changed on an input", () => {
+    //make sure that the hash is unique for any object that has new properties
+    //as js treats different references to the same obj as same and generates the same hash for changed instances too
+    const ash = {};
+    const originalHash = cryptoHash(ash);
+    ash["a"] = "a"; //now if we pass in ash to the cryptohash fxn, it should not be equal to the original hash
+
+    expect(cryptoHash(ash)).not.toEqual(originalHash);
   });
 });
