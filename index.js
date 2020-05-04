8d0d737a3e459c5bf2cfff5265c2b07070e25cc3
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-04 14:35:17 +0545

    made api/transact endpoint

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require("express");
 const request = require("request");
 const Blockchain = require("./blockchain");
 const PubSub = require("./app/pubsub");
+const TransactionPool = require("./wallet/transaction-pool");
+const Wallet = require("./wallet");
 
 const app = express(); //make a local app object that's the result of calling this express fxn
 const blockchain = new Blockchain(); //create a main blockchain for the app [local blockchain instance set to that result]
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -25,6 +29,37 @@ app.post("/api/mine", (req, res) => {
   pubsub.broadcastChain();
 
   res.redirect("/api/blocks"); //give the requestor confirmation that their req was successful by showing the new block added to the chain
+});
+
+//create transactions
+app.post("/api/transact", (req, res) => {
+  const { amount, recipient } = req.body;
+
+  let transaction = transactionPool.existingTransaction({
+    inputAddress: wallet.publicKey,
+  });
+
+  try {
+    //if transaction already exists call update
+    if (transaction) {
+      transaction.update({ senderWallet: wallet, recipient, amount });
+    } else {
+      //create a new trnxn
+      transaction = wallet.createTransaction({
+        recipient,
+        amount,
+        chain: blockchain.chain,
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({ type: "error", message: error.message }); //return will make sure that the rest of the code in the mthd will not apply | status code (400) stands for 'A BAD REQUEST'
+  }
+
+  transactionPool.setTransaction(transaction);
+
+  //pubsub.broadcastTransaction(transaction);
+
+  res.json({ type: "success", transaction });
 });
 
 //to sync the nodes in the ntk
