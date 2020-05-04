3575c76804ce6128c8c668c1c7a19d668f197acc
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-04 14:33:56 +0545

    added  existingTransaction mthd

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -6,6 +6,16 @@ class TransactionPool {
   setTransaction(transaction) {
     this.transactionMap[transaction.id] = transaction;
   }
+
+  existingTransaction({ inputAddress }) {
+    //get an arr of all the transactions in the map
+    const transactions = Object.values(this.transactionMap);
+
+    //return the first item in the arr that matches the incoming input address given to the fxn
+    return transactions.find(
+      (transaction) => transaction.input.address === inputAddress
+    );
+  }
 }
 
 module.exports = TransactionPool;
