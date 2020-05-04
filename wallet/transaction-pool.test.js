41b887084edf77070a7e564eb6c2852f0df6fdfc
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-04 14:34:34 +0545

    tested existingTransaction mthd

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -3,12 +3,13 @@ const Transaction = require("./transaction");
 const Wallet = require("./index");
 
 describe("TransactionPool", () => {
-  let transactionPool, transaction;
+  let transactionPool, transaction, senderWallet;
 
   beforeEach(() => {
     transactionPool = new TransactionPool();
+    senderWallet = new Wallet();
     transaction = new Transaction({
-      senderWallet: new Wallet(),
+      senderWallet,
       recipient: "a-fake-recipient",
       amount: 100,
     });
@@ -19,6 +20,18 @@ describe("TransactionPool", () => {
       transactionPool.setTransaction(transaction);
 
       expect(transactionPool.transactionMap[transaction.id]).toBe(transaction); //toBe() coz we want the transaction in the pool to be the exact transaction obect we're passing into the fxn
+    });
+  });
+
+  describe("existingTransaction()", () => {
+    it("returns an existing transaction given an input address", () => {
+      transactionPool.setTransaction(transaction);
+
+      expect(
+        transactionPool.existingTransaction({
+          inputAddress: senderWallet.publicKey,
+        })
+      ).toBe(transaction);
     });
   });
 });
