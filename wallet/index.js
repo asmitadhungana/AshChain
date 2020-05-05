f65736632e34331eecf3d1ed5d5eb22116f97ce2
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-05 12:29:56 +0545

    added calculateBalance mthd

--- wallet/index.js
+++ wallet/index.js
@@ -22,6 +22,40 @@ class Wallet {
 
     return new Transaction({ senderWallet: this, recipient, amount });
   }
+
+  //calculate wallet's balance
+  static calculateBalance({ chain, address }) {
+    let hasConductedTransaction = false;
+    let outputsTotal = 0; //overall outputs total
+
+    //go through every block in the chain
+    for (let i = chain.length - 1; i > 0; i--) {
+      const block = chain[i];
+
+      //look at every transactions made in the block
+      for (let transaction of block.data) {
+        if (transaction.input.address === address) {
+          hasConductedTransaction = true;
+        }
+
+        const addressOutput = transaction.outputMap[address];
+
+        //if the address has received a tranasaction within the output trnxn map
+        if (addressOutput) {
+          //add that to the outputsTotal
+          outputsTotal = outputsTotal + addressOutput;
+        }
+      }
+
+      if (hasConductedTransaction) {
+        break;
+      }
+    }
+
+    return hasConductedTransaction
+      ? outputsTotal
+      : STARTING_BALANCE + outputsTotal;
+  }
 }
 
 module.exports = Wallet;
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,6 +1,8 @@
 const Wallet = require("./");
 const Transaction = require("./transaction");
 const { verifySignature } = require("../util");
+const Blockchain = require("../blockchain");
+const { STARTING_BALANCE } = require("../config");
 
 describe("Wallet class", () => {
   let wallet;
@@ -72,6 +74,62 @@ describe("Wallet class", () => {
 
       it("outputs the amount to the recipient", () => {
         expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
+    });
+  });
+
+  describe("calculateBalance()", () => {
+    //balance will be calculated based off summing all the transaction made by a wallet throughout the BC history
+    let blockchain;
+
+    beforeEach(() => {
+      blockchain = new Blockchain();
+    });
+
+    //two cases to consider
+    //case I: the wallet has made no trnxns
+    describe("and there are no outputs for the wallet", () => {
+      it("returns the `STARTING_BALANCE`", () => {
+        expect(
+          Wallet.calculateBalance({
+            chain: blockchain.chain,
+            address: wallet.publicKey,
+          })
+        ).toEqual(STARTING_BALANCE);
+      });
+    });
+
+    //caseII : when trnxns have been made from the wallet
+    describe("and there are outputs for the wallet", () => {
+      //we make two trnxns and track how they affect our wallet balance
+      let transactionOne, transactionTwo;
+
+      beforeEach(() => {
+        transactionOne = new Wallet().createTransaction({
+          recipient: wallet.publicKey,
+          amount: 50,
+        });
+
+        transactionTwo = new Wallet().createTransaction({
+          recipient: wallet.publicKey,
+          amount: 60,
+        });
+
+        //record these trnxns in the BC history
+        blockchain.addBlock({ data: [transactionOne, transactionTwo] });
+      });
+
+      it("adds the sum of all outputs from the transactions to the wallet balance", () => {
+        expect(
+          Wallet.calculateBalance({
+            chain: blockchain.chain,
+            address: wallet.publicKey,
+          })
+        ).toEqual(
+          STARTING_BALANCE +
+            transactionOne.outputMap[wallet.publicKey] +
+            transactionTwo.outputMap[wallet.publicKey]
+        );
       });
     });
   });
