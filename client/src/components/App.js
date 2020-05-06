cddee4ef5c8025f0a611ee3c5f1f17c291577f8a
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-06 14:38:13 +0545

    erroroneous commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import { response } from "express";
+import { json } from "body-parser";
 
 class App extends Component {
+  state = { walletInfo: { address: "kakjasdg", balance: 1000 } };
+
+  // componentDidMount() {
+  //   fetch("http://localhost:3000/api/wallet-info")
+  //     .then((response) => response.json())
+  //     .then((json) => this.setState({ walletInfo: json }));
+  // }
+
   render() {
-    return <div>Welcome to the blockchain</div>;
+    const { address, balance } = this.state.walletInfo;
+    return (
+      <div>
+        <div> Welcome to the blockchain</div>
+        <div>Address: {address}</div>
+        <div>Balance: {balance}</div>
+      </div>
+    );
   }
 }
 
