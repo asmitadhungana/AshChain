ddd253eef90a19ed36821adbf2e9c15433f9e1bb
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-05-07 16:06:36 +0545

    Revert "erroroneous commit"
    This reverts commit cddee4ef5c8025f0a611ee3c5f1f17c291577f8a.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,8 @@
 import React, { Component } from "react";
-import { response } from "express";
-import { json } from "body-parser";
 
 class App extends Component {
-  state = { walletInfo: { address: "kakjasdg", balance: 1000 } };
-
-  // componentDidMount() {
-  //   fetch("http://localhost:3000/api/wallet-info")
-  //     .then((response) => response.json())
-  //     .then((json) => this.setState({ walletInfo: json }));
-  // }
-
   render() {
-    const { address, balance } = this.state.walletInfo;
-    return (
-      <div>
-        <div> Welcome to the blockchain</div>
-        <div>Address: {address}</div>
-        <div>Balance: {balance}</div>
-      </div>
-    );
+    return <div>Welcome to the blockchain</div>;
   }
 }
 
